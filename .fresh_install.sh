e0dc8a8620b56d86f9c68ada9ea8773329c36915
Kyle Benson <kyle.edward.benson@gmail.com>
2014-06-01 17:36:58 -0400

    fixed hidden files command

--- .fresh_install.sh
+++ .fresh_install.sh
@@ -1,4 +1,5 @@
 brew update && brew tap jlhonora/lsusb && brew install lsusb
 
 # reveal hidden files
-defaults write com.apple.Finder AppleShowAllFiles YES
+defaults write com.apple.Finder AppleShowAllFiles 1
+killall Finder
